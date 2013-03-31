d560e0cfb0f13898821d94619178bb96b71de367
Paweł Dawczak <pawel.dawczak@gmail.com>
2013-03-31 19:36:20 +0200

    Installed and configured MopaBundle

---
+++ src/App/WebBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
