a612cebf6cb228b7fb35032dc97eddfd88821418
Paweł Dawczak <pawel.dawczak@gmail.com>
2013-04-15 18:15:24 +0200

    Generated Bundle for Pacjent

---
+++ src/App/PacjentBundle/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\PacjentBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Class DefaultController
+ * @package App\PacjentBundle\Controller
+ *
+ * @Route("/pacjent")
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
