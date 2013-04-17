b402d6fd12dbfc9b12b39442dafebcfa79d4132a
Paweł Dawczak <pawel.dawczak@gmail.com>
2013-04-17 18:49:57 +0200

    Added new/create actions; Added new template

--- src/App/PacjentBundle/Controller/DefaultController.php
+++ src/App/PacjentBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace App\PacjentBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use App\PacjentBundle\Form\Type\PacjentType;
 
 /**
  * Class DefaultController
@@ -22,6 +25,43 @@ class DefaultController extends Controller
     {
         return array(
             'pacjenci'  => $this->get('app_pacjent.repository')->findAll(),
+        );
+    }
+
+    /**
+     * @Route("/dodaj", name="app_pacjent_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        return array(
+            'form'  => $this->createForm(new PacjentType())->createView(),
+        );
+    }
+
+    /**
+     * @Route("/dodaj", name="app_pacjent_create")
+     * @Method("POST")
+     * @Template("AppPacjentBundle:Default:new.html.twig")
+     */
+    public function createAction()
+    {
+        $form = $this->createForm(new PacjentType());
+
+        $form->bind($this->getRequest());
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($form->getData());
+            $em->flush();
+
+            $this->get('session')->setFlash('success', 'Dane zpisane poprawnie.');
+            return $this->redirect($this->generateUrl('app_pacjent_index'));
+        }
+
+        return array(
+            'form'  => $form->createView(),
         );
     }
 }
